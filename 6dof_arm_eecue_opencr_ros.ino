f3188f4ce45c2ac808bb66c32b4e16563bcbe0ae
Dave Bullock <eecue@eecue.com>
2022-06-17 15:39:44 -0700

    allocated memry for sequence

--- 6dof_arm_eecue_opencr_ros.ino
+++ 6dof_arm_eecue_opencr_ros.ino
@@ -6,6 +6,7 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
+#include <std_msgs/msg/string.h>
 #include <sensor_msgs/msg/joint_state.h>
 
 
@@ -50,29 +51,36 @@ void error_loop(){
 }
 
 
-void subscription_callback(const void * msgin )
+void subscription_callback_0(const void * msgin )
 {  
- 	const sensor_msgs__msg__JointState * msg = (const sensor_msgs__msg__JointState *)msgin;
-    shoulder.write(150);
-    // Shoulder starts at 80
-    if (msg->position.data[0] > 79){
-      shoulder.write(msg->position.data[0]);
-    }
-    if (msg->position.data[1] > -1){
-      forearm.write(msg->position.data[1]);
-    }
-    if (msg->position.data[2] > -1){
-      elbow.write(msg->position.data[2]);
-    }
-    if (msg->position.data[3] > -1){
-      arm_rotate.write(msg->position.data[3]);
-    }
-    if (msg->position.data[4] > -1){
-      wrist_flex.write(msg->position.data[4]);
-    }
-    if (msg->position.data[5] > -1){
-      wrist_rotate.write(msg->position.data[5]);
-    }
+ 	shoulder.write(150);
+  forearm.write(50);
+  elbow.write(80);
+  arm_rotate.write(32); 
+  wrist_flex.write(20);
+  wrist_rotate.write(100);
+
+  const sensor_msgs__msg__JointState * msg = (const sensor_msgs__msg__JointState *)msgin;
+
+  // Shoulder starts at 80
+  if (msg->position.data[0] > 79){
+    shoulder.write(msg->position.data[0]);
+  }
+  if (msg->position.data[1] > -1){
+    forearm.write(msg->position.data[1]);
+  }
+  if (msg->position.data[2] > -1){
+    elbow.write(msg->position.data[2]);
+  }
+  if (msg->position.data[3] > -1){
+    arm_rotate.write(msg->position.data[3]);
+  }
+  if (msg->position.data[4] > -1){
+    wrist_flex.write(msg->position.data[4]);
+  }
+  if (msg->position.data[5] > -1){
+    wrist_rotate.write(msg->position.data[5]);
+  }
   
 }
 
@@ -93,6 +101,42 @@ void setup() {
   digitalWrite(LED_PIN, HIGH);  
   
 
+  #define STRING_SIZE 100
+  #define MAX_ENTITIES 6
+
+
+  // Init header frame id string
+  msg->header.frame_id.capacity = STRING_SIZE;
+  msg->header.frame_id.size = 0;
+  msg->header.frame_id.data = (char*) malloc(STRING_SIZE * sizeof(char));
+
+  // Init sequence of strings
+  msg->name.capacity = MAX_ENTITIES;
+  msg->name.size = 0;
+  msg->name.data = (rosidl_runtime_c__String*) malloc(MAX_ENTITIES * sizeof(rosidl_runtime_c__String));
+
+  // Init each string of the sequence of strings
+  for(int i = 0; i < MAX_ENTITIES; i++)
+  {
+      msg->name.data[i].capacity = STRING_SIZE;
+      msg->name.data[i].size = 0;
+      msg->name.data[i].data = (char*) malloc(STRING_SIZE * sizeof(char));
+  }
+
+  // Init doubles sequences
+  msg->position.capacity = MAX_ENTITIES;
+  msg->position.size = 0;
+  msg->position.data = (double*) malloc(MAX_ENTITIES * sizeof(double));
+
+  msg->velocity.capacity = MAX_ENTITIES;
+  msg->velocity.size = 0;
+  msg->velocity.data = (double*) malloc(MAX_ENTITIES * sizeof(double));
+
+  msg->effort.capacity = MAX_ENTITIES;
+  msg->effort.size = 0;
+  msg->effort.data = (double*) malloc(MAX_ENTITIES * sizeof(double));
+  
+  
   allocator = rcl_get_default_allocator();
 
   //create init_options
@@ -110,8 +154,10 @@ void setup() {
 
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback_0, ON_NEW_DATA));
 }
+
+
 
 void loop() {
   delay(100);
